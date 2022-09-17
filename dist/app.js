29c77164e1dc12702a7920f75eae58ceba7ac584
Joshua Karp <j.karp@ozemail.com.au>
2022-09-17 18:36:42 +1000

    Can't create separate chatbot.js file and import in app.js - require() does not exist in client-side JS (https://stackoverflow.com/a/19059825)

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,17 @@
 "use strict";
 
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// Entrypoint for the application/widget
+var ChatBot = /*#__PURE__*/_createClass(function ChatBot() {
+  _classCallCheck(this, ChatBot);
+
+  console.log('Chatbot created');
+});
+
 console.log('App is loaded');
+var c = new ChatBot();
--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+// Entrypoint for the application/widget
+class ChatBot {
+  constructor() {
+    console.log('Chatbot created');
+  }
+}
+
 console.log('App is loaded');
+
+const c = new ChatBot();
